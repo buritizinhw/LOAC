4bb46f292648ba276eff7e74b2520b12eb52fa6e
buritizinhw <liviacburiti@outlook.com>
2023-09-06 11:44:11 -0300

    Roteiro 3 oficial

--- LiviaBuriti_Roteiro03PT3.sv
+++ LiviaBuriti_Roteiro03PT3.sv
@@ -5,11 +5,35 @@ module ULA(input logic signed[7:0] A,B,
 
 always_comb begin
     unique case(F)
-        2'b00: SAIDA <= A&B;
-        2'b01: SAIDA <= A|B;
-        2'b10: SAIDA <= A+B;
-        2'b11: SAIDA <= A-B;
+        2'b00:
+            begin 
+            SAIDA <= A&B;
+            FLAG_O <= 0;
+            end
+        2'b01:
+            begin 
+            SAIDA <= A|B;
+            FLAG_O <= 0;
+            end
+        2'b10: 
+            begin
+            SAIDA <= A+B;
+            if(A > 0 & B > 0 & SAIDA < 0 ||  A < 0 & B < 0 & SAIDA > 0)
+                FLAG_O = 1;
+            else 
+                FLAG_O = 0;
+            end
+        2'b11: 
+            begin
+            SAIDA <= A-B;
+            if(A > 0 & B < 0 & SAIDA < 0 || A < 0 & B > 0 & SAIDA > 0 )
+                FLAG_O <= 1;
+            else
+                FLAG_O <= 0;
+            end
     endcase
+    
+
 end
 
 endmodule
